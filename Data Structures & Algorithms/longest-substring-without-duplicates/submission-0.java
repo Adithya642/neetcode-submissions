059b40252aeb72027bd7f68f3e31cb0797c9e5f2
Adithyareddy_k <55428004+Adithya642@users.noreply.github.com>
2026-07-29 19:32:39 -0700

    Add: longest-substring-without-duplicates - submission-0

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.java
@@ -0,0 +1,65 @@
+/*
+I can fix the starting point and create a hashset and store that character and and move my substring ending pointer
+ to right and check is it contains or not if not move my pointer and increase my count , if it contains move my starting point to next character and do the same process again 
+ bRUTE FORCE -[zxyzxyz] 
+ p1-z, p2=x->y->z - maxCount=3 
+ p1=x, p2=y->z->x -maxCount=3
+ p1=y 
+ p1=z 
+ TC- O(n2)
+  Optimal -
+  [zxyzxyz]
+  P1=Z,P2=X ->Y->Z store maxCount 
+  remove the p1 from set and add new character(p2) to set and move p1 to next character repeat this until p2 is string length
+*/
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        HashSet<Character> hs=new HashSet<>();
+        if(s.length()==0) return 0;
+        if(s.length()==1) return 1;
+        int maxLength=0;
+        int p1=0,p2=1;
+        hs.add(s.charAt(p1));
+        while(p2<s.length()){
+          if(!hs.contains(s.charAt(p2))) {
+            maxLength=Math.max(maxLength, p2-p1+1);
+            hs.add(s.charAt(p2));
+            p2++;
+          }
+          else {
+            hs.remove(s.charAt(p1));
+            p1++;
+          }
+        }
+        return maxLength;
+    }
+}
+/*
+zxyzxyz - 
+p1=0,p2=1
+[z] 
+loop - 
+p2=1-x, max=2,[z,x] 
+p2=2-y,max=3,[z,x,y]
+p2=3-z, [x,y], p1=1;
+p2=3-z, max=3, [x,y,z], p2=4
+p2=4-x, [y,z],p1=2 
+p2=4 -x, max=3, [y,z,x] ,p2=5
+p2=5 -y [z,x] p1=3 
+p2=5-y max=3, [z,x,y] p2=6
+p2=6- z ,[x,y] p1=4
+p2=6 -z ,max=3,[x,y,z], p2=7 
+
+
+"xxxx" 
+p1=0,p2=1 
+[x]
+loop -
+p2=1-x , [],p1=1 
+p2=1-x,max=1, [x],p2=2
+p2=2 -x,[],p1=2
+p2=2-x,[x],p2=3
+p2=3-x,[],p1=3
+p2=3-x,[x],p2=4
+
+*/
